a0702320c442db8349c07953b578ad02c08e3a9e
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-12-10 14:21:08 +0000

    Using routes for the different pages of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Games from "./pages/Games/Games"
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
-
   return (
-    <h1>
-      Hello
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+// Use NavLink for the links below later
 
-export default App
+export default App;
