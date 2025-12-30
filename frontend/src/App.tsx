668c8aa6caf5a93e1c9d9b459fa06b41ead66a7c
David Rapu <168489960+davidrapu@users.noreply.github.com>
2025-12-30 15:14:37 +0100

    App.jsx conversion complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Games from "./pages/Games/Games";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 
 function App() {
 
   return (
-    <div>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
