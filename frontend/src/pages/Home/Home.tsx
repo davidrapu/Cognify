b11f6c3a86fb007b19f9aefaa7fb2d47ac967fc9
David Rapu <168489960+davidrapu@users.noreply.github.com>
2025-12-30 16:01:47 +0100

    partially completed home conversion

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { DottedGlowBackground } from "../../components/ui/dotted-glow-background";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <DottedGlowBackground
+        className="pointer-events-none mask-radial-to-90% mask-radial-at-center opacity-80"
+        opacity={1}
+        gap={10}
+        radius={1.6}
+        colorLightVar="--color-neutral-500"
+        glowColorLightVar="--color-neutral-600"
+        colorDarkVar="--color-neutral-500"
+        glowColorDarkVar="--color-sky-800"
+        speedMin={0.3}
+        speedMax={1.6}
+        speedScale={0.8}
+      />
+      <div className="">
+
+      </div>
+    </>
+  );
 }
