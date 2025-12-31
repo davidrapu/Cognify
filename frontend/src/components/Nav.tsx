af0433c1687a3c84012ff22668651517f7da2cf9
David Rapu <168489960+davidrapu@users.noreply.github.com>
2025-12-31 18:05:33 +0100

    Nav almost done. all thats left is the options under user avatar

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,20 +3,25 @@ import {
   NavigationMenu,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
 } from "./ui/navigation-menu";
 import logo from "../assets/icons8-brain-pastel-color-32.png";
 import { NavigationMenuItem } from "@radix-ui/react-navigation-menu";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext/AuthContext";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
 
 const activePageStyle: string =
-  "bg-primary text-primary-foreground text-[1.1em] px-3 py-1 rounded-[0.3em] m-0 transition-all duration-300 ease-in-out font-semibold";
+  "bg-primary text-primary-foreground text-[1.05em] px-3 py-1 rounded-[0.3em] m-0 transition-all duration-300 ease-in-out font-semibold";
 const inactivePageStyle: string =
-  "text-[1em] transition-all duration-300 ease-in-out hover:text-primary hover:text-[1.1em]";
+  "text-[1em] transition-all duration-300 ease-in-out hover:text-primary hover:text-[1.05em]";
 
 export default function Nav() {
+  const { loggedIn } = useAuth();
   return (
-    <NavigationMenu className=" max-h-fit px-6 w-full [&>div]:w-full">
-      <NavigationMenuList className="w-full justify-between pt-5">
+    <NavigationMenu className=" max-h-fit  w-full [&>div]:w-full">
+      <NavigationMenuList className="w-full justify-between mt-5 px-10">
         <div id="pages-and-logo" className="flex gap-x-5">
           <NavigationMenuItem className="">
             <NavigationMenuLink>
@@ -29,7 +34,7 @@ export default function Nav() {
             </NavigationMenuLink>
           </NavigationMenuItem>
 
-          <div id="pages" className="flex gap-x-5 items-center">
+          <div id="pages" className="flex gap-x-4 items-center">
             <NavigationMenuItem>
               <NavigationMenuLink>
                 <NavLink
@@ -83,10 +88,22 @@ export default function Nav() {
             </NavigationMenuItem>
           </div>
         </div>
-        <NavigationMenuItem className="flex items-center mr-3">
-          <NavigationMenuLink>
-            <Link to="/login">Login</Link> // Include signup and logged-in user state later
-          </NavigationMenuLink>
+        <NavigationMenuItem className="flex items-center">
+          {loggedIn ? (
+            <NavigationMenuTrigger className="flex items-center">
+              <Avatar className="border-2 flex items-center justify-center">
+                <AvatarImage src="USER_IMAGE" alt="USER_NAME" />
+                <AvatarFallback>UN</AvatarFallback>
+              </Avatar>
+            </NavigationMenuTrigger>
+          ) : (
+            <NavigationMenuLink>
+              <Link to="/login" className="flex items-center">
+                Login
+              </Link>
+              {/* Include signup and logged-in user state later */}
+            </NavigationMenuLink>
+          )}
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
