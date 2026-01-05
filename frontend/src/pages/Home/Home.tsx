c47867c68da874716114be865dc395ca0721f825
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-06 00:05:40 +0100

    bug of application not showing because background was on a higher layer

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,9 +6,9 @@ import Aims from "./Aims";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative min-h-screen bg-background">
       <DottedGlowBackground
-        className="pointer-events-none mask-radial-to-90% mask-radial-at-center"
+        className="pointer-events-none absolute inset-0 z-0 mask-radial-to-90% mask-radial-at-center"
         opacity={1}
         gap={10}
         radius={1.6}
@@ -17,16 +17,16 @@ export default function Home() {
         colorDarkVar="--color-neutral-500"
         glowColorDarkVar="--color-sky-800"
         speedMin={0.3}
-        speedMax={1.6}
+        speedMax={1}
         speedScale={0.8}
       />
-      <div className="flex flex-col gap-y-28 w-full px-10">
+      <div className="relative z-10 flex flex-col gap-y-10 w-full px-10">
         <Nav />
         <main className="flex flex-col gap-y-36 items-center mt-24">
-          {/* <Hero /> */}
+          <Hero />
           {/* <Aims /> */}
         </main>
       </div>
-    </>
+    </div>
   );
 }
