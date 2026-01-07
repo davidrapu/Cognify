66e5711d0e187508c61018467fec379774870dba
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-07 03:50:28 +0100

    progress bar component

---
+++ frontend/src/components/ui/progress.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type progressPropType = {
+  value: number,
+  max: number
+}
+
+export default function Progress({value, max}: progressPropType) {
+  return (
+    <div
+      role="progressbar"
+      className="h-2 w-full rounded-full bg-primary/20 overflow-hidden"
+    >
+      <div
+        className="h-full bg-primary rounded-full transition-all duration-200 ease-in-out"
+        style={{ width: `${(value / max) * 100}%` }}
+      />
+    </div>
+  );
+}
