ef40042bf2d7630d460f66551fec939ff97275ba
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-08 01:36:41 +0100

    fixed a few bugs

--- frontend/src/pages/Quiz/Quiz.tsx
+++ frontend/src/pages/Quiz/Quiz.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Nav from '../../components/Nav'
 import QuizCard from './QuizCard'
+import { useQuizReducer } from '../../components/hooks/useQuizReducer'
+
 
 export default function Quiz() {
+  const [state, dispatch] = useQuizReducer()
   return (
     <>
     <Header />
-    <main className=' mt-20 flex justify-center min-h-fit '>
-        <QuizCard />
+    <main className=' mt-30 flex justify-center min-h-fit '>
+        <QuizCard state={state} dispatch={dispatch} />
     </main>
     </>
   )
--- frontend/src/pages/Quiz/QuizCard.tsx
+++ frontend/src/pages/Quiz/QuizCard.tsx
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import Progress from "../../components/ui/progress";
 import quizInfo from "../../data/quizInfo.json";
 import Question from "./Question";
+import type { QuizState, QuizAction } from "../../components/hooks/useQuizReducer";
 
-export default function QuizCard() {
-  const [questionNo, setQuestionNo] = useState(1);
+type QuizCardProps = {
+  state: QuizState,
+  dispatch: React.Dispatch<QuizAction>
+}
+
+export default function QuizCard(props: QuizCardProps) {
+  const questionObj = quizInfo.questions.find((question) => question.id === props.state.currentQuestion)
+
+  if (!questionObj) return null
+
   return (
     <div className="container bg-card text-card-foreground w-210 p-5 rounded-[20px] font-black flex flex-col gap-y-7 border border-white/10 ">
       <div className="flex flex-col gap-y-1">
@@ -14,8 +23,11 @@ export default function QuizCard() {
         <p className="text-secondary font-medium">{quizInfo.description}</p>
       </div>
       <div className="flex flex-col gap-y-2">
-        <Progress value={questionNo} max={quizInfo.totalQuestions} />
-        <div className="flex justify-end font-normal">
+        <Progress value={props.state.currentQuestion} max={quizInfo.totalQuestions} />
+        <div className="flex justify-between font-normal">
+          <span>
+            Question <span className="text-primary">{props.state.currentQuestion}</span> of <span className="text-primary">{quizInfo.totalQuestions}</span>
+          </span>
           <span>
             <span className="text-primary">X</span> /
             <span className="text-primary">{quizInfo.totalPoints}</span> points
@@ -23,7 +35,7 @@ export default function QuizCard() {
           </span>
         </div>
       </div>
-      <Question />
+      <Question questionObj={questionObj} />
     </div>
   );
 }
