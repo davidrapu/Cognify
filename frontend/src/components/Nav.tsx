b439b97db8ad4a5f90f154995cfc9b3945d932f9
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-08 17:06:38 +0100

    Navigation link was causing an extra <a> element not needed and was causing errors in the console. So removed

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   NavigationMenu,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "./ui/navigation-menu";
@@ -13,9 +12,9 @@ import { AvatarFallback } from "@radix-ui/react-avatar";
 import Logo from "./Logo";
 
 const activePageStyle: string =
-  "bg-primary text-primary-foreground text-[1.25em] px-3 py-1 rounded-[0.3em] m-0 font-semibold";
+  "bg-primary text-primary-foreground text-lg px-3 py-1 rounded-[0.3em] m-0 font-semibold";
 const inactivePageStyle: string =
-  "text-[1.2em] text-foreground transition-colors duration-150 ease-out hover:text-primary hover:underline underline-offset-4";
+  "text-md text-foreground transition-colors duration-150 ease-out hover:text-primary hover:underline underline-offset-4";
 
 export default function Nav() {
   const { loggedIn } = useAuth();
@@ -24,27 +23,22 @@ export default function Nav() {
       <NavigationMenuList className="w-full justify-between mt-5 px-10">
         <div id="pages-and-logo" className="flex gap-x-5">
           <NavigationMenuItem className="">
-            <NavigationMenuLink>
               <Logo />
-            </NavigationMenuLink>
           </NavigationMenuItem>
 
           <div id="pages" className="flex gap-x-4 items-center">
             <NavigationMenuItem>
-              <NavigationMenuLink>
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    isActive ? activePageStyle : inactivePageStyle
-                  }
-                >
-                  Home
-                </NavLink>
-              </NavigationMenuLink>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? activePageStyle : inactivePageStyle
+                }
+              >
+                Home
+              </NavLink>
             </NavigationMenuItem>
 
             <NavigationMenuItem>
-              <NavigationMenuLink>
                 <NavLink
                   to="/dashboard"
                   className={({ isActive }) =>
@@ -53,11 +47,9 @@ export default function Nav() {
                 >
                   Dashboard
                 </NavLink>
-              </NavigationMenuLink>
             </NavigationMenuItem>
 
             <NavigationMenuItem>
-              <NavigationMenuLink>
                 <NavLink
                   to="/games"
                   className={({ isActive }) =>
@@ -66,11 +58,9 @@ export default function Nav() {
                 >
                   Games
                 </NavLink>
-              </NavigationMenuLink>
             </NavigationMenuItem>
 
             <NavigationMenuItem>
-              <NavigationMenuLink>
                 <NavLink
                   to="/about"
                   className={({ isActive }) =>
@@ -79,26 +69,28 @@ export default function Nav() {
                 >
                   About
                 </NavLink>
-              </NavigationMenuLink>
             </NavigationMenuItem>
           </div>
         </div>
         <NavigationMenuItem className="flex items-center">
-          {loggedIn ? (
+          {loggedIn ? 
+          (
             <NavigationMenuTrigger className="flex items-center">
               <Avatar className="border-2 flex items-center justify-center">
                 <AvatarImage src="USER_IMAGE" alt="USER_NAME" />
                 <AvatarFallback>UN</AvatarFallback>
               </Avatar>
             </NavigationMenuTrigger>
-          ) : (
-            <NavigationMenuLink>
-              <Link to="/login" className="flex items-center text-foreground text-[1.2em] ">
+          ) : 
+          (
+              <Link
+                to="/login"
+                className="flex items-center text-foreground text-md "
+              >
                 Login
               </Link>
-              {/* Include signup and logged-in user state later */}
-            </NavigationMenuLink>
-          )}
+          )
+          }
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
--- frontend/src/components/ui/navigation-menu.tsx
+++ frontend/src/components/ui/navigation-menu.tsx
@@ -122,22 +122,6 @@ function NavigationMenuViewport({
   )
 }
 
-function NavigationMenuLink({
-  className,
-  ...props
-}: React.ComponentProps<typeof NavigationMenuPrimitive.Link>) {
-  return (
-    <NavigationMenuPrimitive.Link
-      data-slot="navigation-menu-link"
-      className={cn(
-        "data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus-visible:ring-ring/50 [&_svg:not([class*='text-'])]:text-muted-foreground flex gap-1 rounded-sm text-sm transition-all outline-none focus-visible:ring-[3px] focus-visible:outline-1 [&_svg:not([class*='size-'])]:size-4",
-        className
-      )}
-      {...props}
-    />
-  )
-}
-
 function NavigationMenuIndicator({
   className,
   ...props
@@ -162,8 +146,6 @@ export {
   NavigationMenuItem,
   NavigationMenuContent,
   NavigationMenuTrigger,
-  NavigationMenuLink,
   NavigationMenuIndicator,
   NavigationMenuViewport,
-  navigationMenuTriggerStyle,
 }
