46d9cec0a391ee213556bd5f1023b77bc95ee594
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-09 20:15:18 +0100

    input is determined by the question type, UX design fix

--- frontend/src/pages/Quiz/Question.tsx
+++ frontend/src/pages/Quiz/Question.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useRef, useEffect } from "react";
 
 type QuestionData = {
   id: number,
@@ -15,18 +15,21 @@ type QuestionProps = {
 }
 
 export default function Question({questionObj, userInput, onChange} : QuestionProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(e.target.value)
-  }
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [questionObj.id]); // focus when question changes
 
   return (
     <div className="w-full h-full flex flex-col gap-y-3 ">
       <h1 className="text-2xl">{questionObj.question}</h1>
       <input
         value={userInput}
-        onChange={handleChange}
+        ref={inputRef}
+        onChange={(e) => onChange(e.target.value)}
         placeholder="Answer..."
+        type={questionObj.type === "calculation" ? "number" : "text"}
         className=" bg-input p-1 rounded-[10px] text-foreground font-normal focus:outline-none focus:ring-2 focus:ring-primary "
       />
     </div>
