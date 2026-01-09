869981a89aa0a1e4ed00443493a9ac69e15d1262
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-09 19:56:28 +0100

    folder structure fix up

--- frontend/src/hooks/useQuizReducer.ts
+++ frontend/src/hooks/useQuizReducer.ts
@@ -1,17 +1,21 @@
 import { useReducer } from "react";
 
 const initialState = {
-  currentQuestion: 1
+  currentQuestion: 1,
+  totalPoints: 0
 };
 export type QuizState = {
   currentQuestion: number;
+  totalPoints: number;
 };
-export type QuizAction = { type: "next"};
+export type QuizAction = { type: "next"} | {type: "increaseTotalPoints", payload: number};
 
 function reducer(state: QuizState, action: QuizAction) {
   switch (action.type) {
     case "next":
       return { ...state, currentQuestion: state.currentQuestion + 1 };
+    case "increaseTotalPoints":
+      return {...state, totalPoints: state.totalPoints + action.payload}
     default:
       return state;
   }
