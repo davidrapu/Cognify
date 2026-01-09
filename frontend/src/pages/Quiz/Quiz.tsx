1eec96325ad54cdbc2794b9c5c0b5b4d6d3233b8
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-09 20:17:11 +0100

    vscode changes

--- frontend/src/pages/Quiz/Quiz.tsx
+++ frontend/src/pages/Quiz/Quiz.tsx
@@ -1,18 +1,17 @@
-import Nav from '../../components/Nav'
-import QuizCard from './QuizCard'
-import { useQuizReducer } from '../../components/hooks/useQuizReducer'
-
+import Nav from "../../components/Nav";
+import QuizCard from "./QuizCard";
+import { useQuizReducer } from "../../hooks/useQuizReducer";
 
 export default function Quiz() {
-  const [state, dispatch] = useQuizReducer()
+  const [state, dispatch] = useQuizReducer();
   return (
     <>
-    <header>
-      <Nav />
-    </header>
-    <main className=' mt-30 flex justify-center min-h-fit '>
+      <header>
+        <Nav />
+      </header>
+      <main className=" mt-30 flex justify-center min-h-fit ">
         <QuizCard state={state} dispatch={dispatch} />
-    </main>
+      </main>
     </>
-  )
+  );
 }
