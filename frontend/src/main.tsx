a4883a0768a58907ff66a5b4605ee5459dee16ce
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-14 00:56:07 +0000

    trying speed insights with vercel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext/AuthContextProvider.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <SpeedInsights />
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
