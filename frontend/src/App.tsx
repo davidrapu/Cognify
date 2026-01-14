6ad2d29111d5f67b87d176b0e9283dfdb352b1c1
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-14 00:59:23 +0000

    speed insights bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Games from "./pages/Games/Games";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Quiz from "./pages/Quiz/Quiz";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/games" element={<Games />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/quiz" element={<Quiz />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+      <SpeedInsights />
+    </>
   );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext/AuthContextProvider.tsx";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SpeedInsights />
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
