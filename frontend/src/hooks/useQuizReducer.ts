73f4a2170f89c6457ce6789b9b3cf96cf12c7160
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-15 23:59:18 +0000

    added quizeState state

--- frontend/src/hooks/useQuizReducer.ts
+++ frontend/src/hooks/useQuizReducer.ts
@@ -2,13 +2,15 @@ import { useReducer } from "react";
 
 const initialState = {
   currentQuestion: 1,
-  totalPoints: 0
+  totalPoints: 0,
+  quizState: 'intro' // Intro | Active | Inactive
 };
 export type QuizState = {
   currentQuestion: number;
   totalPoints: number;
+  quizState: string;
 };
-export type QuizAction = { type: "next"} | {type: "increaseTotalPoints", payload: number};
+export type QuizAction = { type: "next"} | {type: "increaseTotalPoints", payload: number} | {type: 'start'} | {type: 'end'} | {type: 'reset'};
 
 function reducer(state: QuizState, action: QuizAction) {
   switch (action.type) {
@@ -16,6 +18,12 @@ function reducer(state: QuizState, action: QuizAction) {
       return { ...state, currentQuestion: state.currentQuestion + 1 };
     case "increaseTotalPoints":
       return {...state, totalPoints: state.totalPoints + action.payload}
+    case 'start':
+      return {...state, quizState: 'Active'}
+    case 'end':
+      return {...state, quizState: 'Inactive'}
+    case 'reset':
+      return {...state, quizState: 'Intro'}
     default:
       return state;
   }
