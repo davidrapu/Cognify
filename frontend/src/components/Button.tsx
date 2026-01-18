a1d0f82fee59ea72167197f4fddf035b53442749
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-18 22:42:23 +0000

    creating a reusable button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Button() {
+  return (
+    <button className="bg-primary text-primary-foreground tracking-wide w-fit py-1 px-3 rounded-[5px] self-end cursor-pointer disabled:cursor-not-allowed disabled:bg-secondary"></button>
+  );
+}
