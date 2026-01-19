272aad463b83bb65bfab5c06e4c79135780fac82
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-19 14:13:42 +0000

    reusable button component, not fully complete

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,8 +1,22 @@
+import React from "react";
+import { cn } from "@/lib/utils";
 
+interface ButtonProps extends React.PropsWithChildren{
+  className?: string
+  disabled?: boolean
+  onClick?: () => void
+}
 
-
-export default function Button() {
+export default function Button({children, className = '', disabled = false, onClick} : ButtonProps) {
   return (
-    <button className="bg-primary text-primary-foreground tracking-wide w-fit py-1 px-3 rounded-[5px] self-end cursor-pointer disabled:cursor-not-allowed disabled:bg-secondary"></button>
+    <button
+      className={cn(
+        "bg-primary text-primary-foreground tracking-wide py-1 px-3 rounded cursor-pointer disabled:cursor-not-allowed disabled:bg-secondary", className,
+      )}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
