bc4d71faaf6919223e759098a598fcc86ad1c109
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-20 21:10:49 +0000

    better reusable button pattern

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-interface ButtonProps extends React.PropsWithChildren{
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   className?: string
-  disabled?: boolean
-  onClick?: () => void
 }
 
-export default function Button({children, className = '', disabled = false, onClick} : ButtonProps) {
+export default function Button({children, className = '', ...props } : ButtonProps) {
   return (
     <button
       className={cn(
         "bg-primary text-primary-foreground tracking-wide py-1 px-3 rounded cursor-pointer disabled:cursor-not-allowed disabled:bg-secondary", className,
       )}
-      disabled={disabled}
-      onClick={onClick}
+      {...props}
     >
       {children}
     </button>
