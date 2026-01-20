503c6dec63c7b1e1241d75abe8972939c715f461
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-01-20 21:12:21 +0000

    included the timer for action questions

--- frontend/src/pages/Quiz/Question/ActionQuestion.tsx
+++ frontend/src/pages/Quiz/Question/ActionQuestion.tsx
@@ -1,23 +1,47 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Button from '@/components/Button'
 
 interface ActionQuestionProps {
     question: string
     comment?: string
+    setUserInput: React.Dispatch<React.SetStateAction<string>>
+    userInput: string
 }
 
-export default function ActionQuestion({question, comment} : ActionQuestionProps) {
+export default function ActionQuestion({question, comment, setUserInput, userInput} : ActionQuestionProps) {
     const [isActive, setIsActive] = useState(false)
-    const handleClick = () => {
-        setIsActive((isActive) => !isActive)
+    const [time, setTime] = useState(0.0)
+
+    const intervalRef = useRef<number | null>(null)
+
+    const startTimer = () => {
+
+      if (intervalRef.current !== null) return
+
+      setIsActive(true);
+
+      intervalRef.current = window.setInterval(() => {
+        setTime(prev => prev + 0.01)
+      }, 10)
     }
+
+    const stopTimer = () => {
+      if (intervalRef.current !== null){
+        clearInterval(intervalRef.current)
+        intervalRef.current = null
+      }
+      setIsActive(false)
+      setTime(0)
+      setUserInput( time.toString() )
+    }
+
     return (
       <div className="w-full h-full flex flex-col gap-y-3 ">
         <div>
           <h1 className="text-2xl">{question}</h1>
           <p className="font-medium text-secondary"> {comment} </p>
         </div>
-        <Button onClick={handleClick} className={`self-center rounded-[50%] size-30 mt-3 text-xl font-semibold ${isActive ? 'bg-destructive' : ''} `}>Click me</Button>
+        <Button disabled={userInput.trim() !== '' && isActive === false } onClick={isActive ? stopTimer : startTimer} className={`self-center rounded-[50%] size-30 mt-3 text-xl font-semibold ${isActive ? 'bg-destructive' : ''} `}>{isActive ? "Stop" : "Start"}</Button>
       </div>
     )
 }
