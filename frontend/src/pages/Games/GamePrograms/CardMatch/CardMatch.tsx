c0152c0cd38e6ea2891152aa3091e650153119ba
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-05 04:17:23 +0000

    Integrates animation and dispatch logic to card match game
    Adds framer-motion for entry animation, enhances game state management by introducing dispatch callbacks for attempt and match tracking, and refines card selection logic to improve user experience and enable external state tracking.

--- frontend/src/pages/Games/GamePrograms/CardMatch/CardMatch.tsx
+++ frontend/src/pages/Games/GamePrograms/CardMatch/CardMatch.tsx
@@ -1,24 +1,33 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 import cardsObj from "@/data/cards.json";
+import { motion } from "framer-motion";
+import {type CardMatchAction} from "@/hooks/useCardMatchReducer";
 
 const shuffledCards = cardsObj.cards.sort(() => Math.random() - 0.5);
-export default function CardMatch() {
+
+type CardMatchProps = {
+    dispatch: React.Dispatch<CardMatchAction>;
+}
+
+export default function CardMatch({dispatch}: CardMatchProps) {
   const [cards, setCards] = useState<typeof cardsObj.cards>(shuffledCards);
   const [choiceOne, setChoiceOne] = useState<number | null>(null);
   const [choiceTwo, setChoiceTwo] = useState<number | null>(null);
-
 
   const handleChoice = (id: number) => {
     if (choiceTwo !== null) return; // Prevent selecting more than two cards
 
     if (choiceOne === null) setChoiceOne(id);
-    else setChoiceTwo(id);
-  }
+    else {
+      setChoiceTwo(id)
+      dispatch({ type: "increaseAttempts" });
+    }
+  };
   const reset = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
-  }
+  };
 
   useEffect(() => {
     if (!choiceOne || !choiceTwo) return;
@@ -29,29 +38,36 @@ export default function CardMatch() {
     ];
 
     if (firstCard?.value === secondCard?.value) {
-      // Matche found
+      // Match found
       setCards((prevCards) => {
         return prevCards.map((card) => {
-          if ((firstCard && card.id === firstCard.id) || (secondCard && card.id === secondCard.id)) {
+          if (
+            (firstCard && card.id === firstCard.id) ||
+            (secondCard && card.id === secondCard.id)
+          ) {
             return { ...card, matched: true };
           }
           return card;
         });
       });
       reset();
-    }
-    else {
+      setTimeout(() => dispatch({ type: "increaseMatchedCards" }), 250);
+      
+    } else {
       setTimeout(() => reset(), 600);
     }
-  }, [choiceOne, choiceTwo, cards]);
-
+  }, [choiceOne, choiceTwo, cards, dispatch]);
   const handleFlip = (id: number) => {
     handleChoice(id);
-  }
-
+  };
 
   return (
-    <main className="p-10 border-2 border-red flex flex-col ">
+    <motion.div
+      initial={{ opacity: 0, x: 30 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1.2 }}
+      className="px-30 py-10 rounded-[30px] bg-secondary/25"
+    >
       <div className="grid grid-cols-4 gap-4 w-fit justify-items-center mx-auto">
         {Array.from({ length: 16 }).map((_, i) => (
           <Card
@@ -63,6 +79,6 @@ export default function CardMatch() {
           />
         ))}
       </div>
-    </main>
+    </motion.div>
   );
 }
