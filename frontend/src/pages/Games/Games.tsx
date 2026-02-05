adb8d2a3e12a3051deeea061716d3c0a147f0ab0
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-05 04:16:23 +0000

    Removes Nav component from Games page for cleaner layout

--- frontend/src/pages/Games/Games.tsx
+++ frontend/src/pages/Games/Games.tsx
@@ -1,12 +1,8 @@
-import Nav from "@/components/Nav";
 import { Outlet } from "react-router-dom";
 
 export default function Games() {
   return (
     <div className="flex flex-col gap-y-10">
-      <header>
-        <Nav />
-      </header>
       <Outlet />
     </div>
   );
