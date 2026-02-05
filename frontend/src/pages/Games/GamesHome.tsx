381d856f5c18e24b697045d0a913552379dbdb54
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-05 04:16:38 +0000

    Adds Nav component to GamesHome for improved layout structure

--- frontend/src/pages/Games/GamesHome.tsx
+++ frontend/src/pages/Games/GamesHome.tsx
@@ -1,10 +1,16 @@
 // import Button from "@/components/Button"
 // import { useNavigate } from "react-router-dom"
 
+import Nav from "@/components/Nav";
+
 export default function GamesHome() {
     // const navigate = useNavigate()
   return (
-    <main className="">
-    </main>
-  )
+    <>
+      <header>
+        <Nav />
+      </header>
+      <main className=""></main>
+    </>
+  );
 }
