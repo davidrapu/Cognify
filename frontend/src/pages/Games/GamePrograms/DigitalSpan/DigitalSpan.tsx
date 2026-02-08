723903e24b67241a8f954c4159b6ed8aaafc311a
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-08 07:33:24 +0000

    refactor: replace EmptyPage with animated layout in DigitalSpan component

--- frontend/src/pages/Games/GamePrograms/DigitalSpan/DigitalSpan.tsx
+++ frontend/src/pages/Games/GamePrograms/DigitalSpan/DigitalSpan.tsx
@@ -1,7 +1,12 @@
-import EmptyPage from "@/components/EmptyPage";
+
+import { motion } from "framer-motion";
 
 export default function DigitalSpan() {
   return (
-    <EmptyPage />
+    <div className="flex flex-1 w-full items-center justify-center backdrop-blur-sm drop-shadow-xl/30">
+      <motion.div initial={{scale: 0}} animate={{scale: 1}} exit={{scale: 0}} transition={{ duration: 0.7 }} className="bg-secondary min-h-170 aspect-video rounded-2xl border">
+
+      </motion.div>
+    </div>
   )
 }
