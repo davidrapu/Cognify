47dd71c0aecc095e019ea3541c22bffaafd7f10c
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-10 17:50:06 +0000

    Adds interactive sequence recall game state
    Introduces an active gameplay mode for sequence recall,
    allowing users to view numbers sequentially, enter responses,
    and receive feedback. Improves user engagement by moving
    beyond static content.

--- frontend/src/pages/Games/GamePrograms/SequenceRecall/SequenceRecall.tsx
+++ frontend/src/pages/Games/GamePrograms/SequenceRecall/SequenceRecall.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import Active from "./states/Active";
 import EmptyPage from "@/components/EmptyPage";
 
 export default function SequenceRecall() {
+  const [state, setState] = useState<"start" | "active" | "end">("active");
   return (
-    <EmptyPage />
-  )
+    <>
+      {state === "active" && <Active />}
+      {state === "start" && <EmptyPage />}
+    </>
+  );
 }
