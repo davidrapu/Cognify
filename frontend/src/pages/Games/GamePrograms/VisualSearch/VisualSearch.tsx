6fec9c46a54dd62a31565c80fe71409df568e8e4
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-12 04:20:41 +0000

    Adds visual search game with dynamic color challenge
    Introduces a new interactive game mode focused on identifying subtly different colors under varying difficulty levels.
    
    Enhances user engagement by tracking score, handling incorrect selections, and offering a reset option for repeated play.

---
+++ frontend/src/pages/Games/GamePrograms/VisualSearch/VisualSearch.tsx
@@ -0,0 +1,12 @@
+import EmptyPage from "@/components/EmptyPage";
+import Active from "./states/Active";
+
+const state: "intro" | "active" | "end" = "active";
+export default function VisualSearch() {
+  return (
+    <>
+      {state === "intro" && <EmptyPage />}
+      {state === "active" && <Active difficulty="medium" />}
+    </>
+  );
+}
