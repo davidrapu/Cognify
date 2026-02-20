56cf3ff1c55f581129274b89ad0413146361c667
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-20 11:35:23 +0000

    Adds user avatar to games page header
    Introduces a user avatar component in the page header for improved personalization and visual context. Refactors layout for better alignment, spacing, and scroll handling.

--- frontend/src/pages/Games/Games.tsx
+++ frontend/src/pages/Games/Games.tsx
@@ -1,4 +1,5 @@
 import AppBar from "@/components/AppBar/AppBar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   SidebarInset,
   SidebarProvider,
@@ -8,12 +9,18 @@ import { Outlet } from "react-router";
 
 export default function Games() {
   return (
-    <div className="w-screen">
+    <div className="w-full min-h-screen overflow-x-hidden">
       <SidebarProvider>
         <AppBar />
-        <SidebarInset>
-          <SidebarTrigger className="ml-1 text-foreground" />
-          <div className="flex-1">
+        <SidebarInset className="flex flex-col min-h-screen">
+          <div className="flex justify-between pr-2 pt-2 mb-2">
+            <SidebarTrigger className="m-1 text-foreground" />
+            <Avatar className="border-2 size-9 flex items-center justify-center">
+              <AvatarImage src="USER_IMAGE" alt="USER_NAME" />
+              <AvatarFallback>UN</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="flex-1 overflow-y-auto">
             <Outlet />
           </div>
         </SidebarInset>
