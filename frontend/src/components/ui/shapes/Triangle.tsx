1e9c0e249924db39f52f916abe4b574cdd100a19
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-20 11:34:33 +0000

    reusable triangle component. to be used later

---
+++ frontend/src/components/ui/shapes/Triangle.tsx
@@ -0,0 +1,8 @@
+export function TriangleUp() {
+    return (
+
+        <div className="w-0 h-0 border-l-50 border-l-transparent border-r-50 border-r-transparent border-b-100 border-b-primary">
+
+        </div>
+    )
+}
