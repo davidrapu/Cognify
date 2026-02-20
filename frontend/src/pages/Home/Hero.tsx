e3380f5b967c3218df7b79e24ce09d0285cffef4
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-20 11:35:58 +0000

    Refactors Hero component layout and adjusts gap in Home component

--- frontend/src/pages/Home/Hero.tsx
+++ frontend/src/pages/Home/Hero.tsx
@@ -1,4 +1,4 @@
-import {AnimatedButton} from "@/components/Button";
+import { AnimatedButton } from "@/components/Button";
 import { useAuth } from "../../contexts/AuthContext/AuthContext";
 import { useNavigate } from "react-router";
 
@@ -7,18 +7,18 @@ export default function Hero() {
   const navigate = useNavigate();
 
   return (
-    <section className="flex flex-col items-center gap-y-6 md:gap-y-8 animate-in slide-in-from-bottom-20 fade-in duration-500">
-      <div className="flex flex-col items-center">
-        <h1 className="text-8xl font-(family-name:--headings) tracking-[0.2em] text-foreground leading-[0.9] m-0">
-          WELCOME
-        </h1>
-        <p className="pt-2 text-center text-lg text-secondary-text tracking-widest leading-[1.4]">
-          Hi, welcome to Cognify. Helping you think sharper, every day.
-        </p>
+    <section className="flex flex-col w-full items-center justify-center gap-y-6 animate-in slide-in-from-bottom-20 fade-in duration-500">
+      <div className="flex flex-col items-center w-fit z-20">
+          <h1 className="text-8xl font-family-heading tracking-[0.2em] text-foreground leading-[0.9] m-0">
+            WELCOME
+          </h1>
+          <p className="pt-2 text-center text-lg text-secondary-text tracking-widest leading-[1.4]">
+            Hi, welcome to Cognify. Helping you think sharper, every day.
+          </p>
       </div>
-      <AnimatedButton
-        onClick={() => navigate(loggedIn ? "/dashboard" : "/quiz")}
-        className="
+        <AnimatedButton
+          onClick={() => navigate(loggedIn ? "/dashboard" : "/quiz")}
+          className="
           self-center
           text-xl
           py-[0.3em] px-[1em]
@@ -27,9 +27,9 @@ export default function Hero() {
           transition-shadow duration-200 ease-out
           focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary/60
         "
-      >
-        Get Started
-      </AnimatedButton>
+        >
+          Get Started
+        </AnimatedButton>
     </section>
   );
 }
