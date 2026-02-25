64435900205e90e0abb18514740a3884a5a0b934
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-25 08:05:43 +0000

    Adds login form handling and authentication flow
    Implements state management for login credentials and error handling.
    Integrates authentication and redirects user upon successful login.
    Improves user experience by providing feedback on failed attempts.

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,10 +1,48 @@
 import { LoginForm } from "@/components/login-form";
+import { useAuth } from "@/contexts/AuthContext/AuthContext";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Login() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const [error, setError] = useState<boolean>(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({ username: email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      setEmail(null)
+      setPassword(null)
+      setError(true);
+      console.log(error, 'Username or password is incorrect');
+      return;
+    }
+
+    // const data = await response.json();
+    login()
+    navigate("/dashboard");
+  };
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
-        <LoginForm />
+        <LoginForm
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          handleSubmit={handleSubmit}
+        />
       </div>
     </div>
   );
