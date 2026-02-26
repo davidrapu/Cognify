a79db60970e604fe66d8605c4edcfa37daca78f6
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-26 20:46:53 +0000

    fix(auth): update CORS configuration and change server port to 4000

--- backend/auth/src/authServer.ts
+++ backend/auth/src/authServer.ts
@@ -3,14 +3,18 @@ const cors = require("cors");
 const errorHandler = require("./middleware/error");
 const notFound = require("./middleware/notFound");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const { login, register, logout, refreshToken } = require("./controller/auth.controller");
 import type { Request, Response } from "express";
 
 const app = express();
 
+const allowedOrigin = process.env.NODE_ENV === "production" ? process.env.PRODUCTION_FRONTEND_URL : "http://localhost:5173";
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigin,
+    credentials: true,
   }),
 );
 app.use(express.json());
@@ -22,13 +26,10 @@ app.post("/register", register)
 app.post("/logout", logout)
 app.post("/token", refreshToken)
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to the Auth API" });
-})
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(6000, () => {
-  console.log("Server is running on port 6000");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
