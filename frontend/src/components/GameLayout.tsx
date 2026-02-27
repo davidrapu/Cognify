fb1919a3dc97181a26cb74a9daa08913e58a4bd9
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-27 18:08:18 +0000

    removing the testing border

--- frontend/src/components/GameLayout.tsx
+++ frontend/src/components/GameLayout.tsx
@@ -1,7 +1,7 @@
 
 export default function GameLayout({children}: {children: React.ReactNode}) {
   return (
-    <div className="flex min-h-full border justify-center backdrop-blur-sm">
+    <div className="flex min-h-full justify-center backdrop-blur-sm">
       <div className="lg:min-h-150 md:min-h-100 sm:min-h-500 flex flex-col self-center drop-shadow-xl/30 bg-secondary md:aspect-video rounded-2xl p-5 gap-y-3 animate-in zoom-in-0 duration-300">{children}</div>
     </div>
   );
