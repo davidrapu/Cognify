6307fcbaf03dd393ec12738f1cdcd0c853a2b699
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-27 21:22:49 +0000

    implementing shadcn dark mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext/AuthContextProvider.tsx";
+import { ThemeProvider } from "./contexts/Theme/ThemeContextProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
+      </ThemeProvider>
     </AuthContextProvider>
   </StrictMode>
 );
