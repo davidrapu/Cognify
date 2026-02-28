bd5b74ff68c1322efcec3ef62daa0f0eee6bf9a8
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-28 22:47:50 +0000

    reworking game layout

--- frontend/src/components/GameLayout.tsx
+++ frontend/src/components/GameLayout.tsx
@@ -1,8 +1,16 @@
+import { cn } from "@/lib/utils";
 
-export default function GameLayout({children}: {children: React.ReactNode}) {
+export default function GameLayout({children, animateIn = true}: {children: React.ReactNode, animateIn?: boolean}) {
   return (
     <div className="flex min-h-full justify-center backdrop-blur-sm">
-      <div className="lg:min-h-150 md:min-h-100 sm:min-h-500 flex flex-col self-center drop-shadow-xl/30 bg-secondary md:aspect-video rounded-2xl p-5 gap-y-3 animate-in zoom-in-0 duration-300">{children}</div>
+      <div
+        className={cn(
+          " md:w-285 md:aspect-video flex flex-col self-center drop-shadow-xl/30 bg-secondary rounded-2xl p-5 gap-y-3",
+          animateIn && "animate-in zoom-in-0 duration-300"
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 }
