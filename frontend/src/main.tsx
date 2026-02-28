9fc3b300aa751d99080314c9006d27c514f6fa33
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-28 22:46:53 +0000

    using shadcn tool tip

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext/AuthContextProvider.tsx";
 import { ThemeProvider } from "./contexts/Theme/ThemeContextProvider.tsx";
+import { TooltipProvider } from "./components/ui/tooltip.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthContextProvider>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <App />
+        <TooltipProvider>
+          <App />
+        </TooltipProvider>
       </ThemeProvider>
     </AuthContextProvider>
   </StrictMode>
