670f2a7b28d0037ea734f78c1b87198df2ee1ded
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-28 22:51:42 +0000

    Remove ModeToggle component from Home page for cleaner layout

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,16 @@
 import Nav from "../../components/Nav";
 import Hero from "./Hero";
 import Aims from "./Aims";
-import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-    <div className="relative z-10 flex flex-col gap-y-5 w-full">
+    <div className="flex flex-col gap-y-5 w-full">
       <header>
         <Nav />
       </header>
       <main className="relative flex flex-col gap-y-36 items-center mt-24">
         <Hero />
         <Aims />
-        <ModeToggle />
       </main>
     </div>
   );
