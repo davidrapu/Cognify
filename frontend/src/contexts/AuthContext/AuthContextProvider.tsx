1b3933c00e3e1bd771a866cf2501f4ab7630e32f
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-02-28 22:49:16 +0000

    for later use of user data

--- frontend/src/contexts/AuthContext/AuthContextProvider.tsx
+++ frontend/src/contexts/AuthContext/AuthContextProvider.tsx
@@ -7,8 +7,16 @@ type AuthContextProviderProps = {
     children: React.ReactNode;
 }
 
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 export function AuthContextProvider(props: AuthContextProviderProps) {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
   const login = () => setLoggedIn(true);
   const logout = () => setLoggedIn(false);
