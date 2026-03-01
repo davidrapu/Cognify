7ed71139f325fb81d7a0eb2604127370a893ff19
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-01 20:19:47 +0000

    Refactor authentication middleware to handle errors with custom error messages

--- backend/main/src/middleware/authentication.ts
+++ backend/main/src/middleware/authentication.ts
@@ -1,12 +1,22 @@
 import type { Request, Response, NextFunction } from "express";
+import type errorsType = require("../types/errorsType");
 const jwt = require("jsonwebtoken");
+
 function authenticateToken(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies.accessToken;
 
-  if (token == null) return res.sendStatus(401);
+  if (token == null) {
+    const err: errorsType.HttpError = new Error("No token provided");
+    err.status = 401;
+    return next(err);
+  };
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err : any, user: any) => {
-      if (err) return res.sendStatus(403);
+      if (err) {
+        const err: errorsType.HttpError = new Error("Invalid token");
+        err.status = 403;
+        return next(err);
+      }
       
       req.user = user;
       next();
