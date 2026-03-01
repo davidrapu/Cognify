413ef9e7f8264d5feeb62e1ebf44cce7514eb34e
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-01 20:19:58 +0000

    Add SessionData interface to define session-related data structure

--- backend/main/src/types/dataTypes.ts
+++ backend/main/src/types/dataTypes.ts
@@ -1,3 +1,16 @@
 export interface UserData {
   id: number;
 };
+
+export interface SessionData {
+  id: number;
+  userId: number;
+  gameName: string;
+  correct: number;
+  incorrect: number;
+  reactionTimeAvg: number;
+  reactionTimeStd: number;
+  duration: number;
+  domain: string;
+  date: string;
+}
