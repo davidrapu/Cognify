ba11b903c438b9c630c4f002167f0d6a333df017
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-13 06:00:44 +0000

    Initializes Prisma with PostgreSQL support
    Sets up database schema, migrations, and config for PostgreSQL.
    Enables environment-based configuration and prepares user authentication model.
    Lays foundation for scalable database integration.

---
+++ backend/database/lib/prisma.ts
@@ -0,0 +1,14 @@
+import { configDotenv } from "dotenv";
+if (process.env.ENV !== "production") {
+  configDotenv({ path: "../../.env" });
+}
+
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { PrismaClient } = require("../generated/prisma/client");
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+module.exports = { prisma };
