30505632e9740c33eef82751a107fbd203777456
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-15 20:43:03 +0000

    drop down icon not needed here

--- frontend/src/components/ui/navigation-menu.tsx
+++ frontend/src/components/ui/navigation-menu.tsx
@@ -73,11 +73,6 @@ function NavigationMenuTrigger({
       {...props}
     >
       {children}{" "}
-      {/* <ChevronDownIcon
-        className="relative top-px ml-1 size-3 transition ease-linear duration-300 group-data-[state=open]:rotate-180"
-        aria-hidden="true"
-        // Remove this icon after adding the content
-      /> */}
     </NavigationMenuPrimitive.Trigger>
   )
 }
