6715a3c5ad410dde2b91afeddabbf13dea1d4f1f
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-16 04:04:09 +0000

    refactor: update user repository and authentication middleware for improved structure and error handling

--- backend/src/database/repositories/user.repository.ts
+++ backend/src/database/repositories/user.repository.ts
@@ -1,20 +1,26 @@
-import { hash } from "node:crypto";
+import { PrismaClient } from "../generated/prisma/client";
 
-const {prisma} = require("../lib/prisma");
-async function createNewUser(firstName: string, lastName: string, email: string, hash: string) {
+const {prisma}: {prisma:PrismaClient} = require("../lib/prisma");
+
+async function createNewUser(
+  firstName: string,
+  lastName: string,
+  email: string,
+  hash: string,
+) {
   return prisma.user.create({
     data: {
       firstName,
       lastName,
       email,
-      hash
+      hash,
     },
     select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true
-    }
+      id: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+    },
   });
 }
 
@@ -22,29 +28,29 @@ async function getUserByEmail(email: string) {
   return prisma.user.findUnique({
     where: { email },
     select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true,
-        hash: true
-    }
+      id: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      hash: true,
+    },
   });
 }
 
-async function getUserById(id: number) {
+async function getUserById(id: string) {
   return prisma.user.findUnique({
     where: { id },
     select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true
-    }
+      id: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+    },
   });
 }
 
 async function getUsers() {
-  return prisma.user.findMany()
+  return prisma.user.findMany();
 }
 
 async function deleteUserByEmail(email: string) {
@@ -54,9 +60,9 @@ async function deleteUserByEmail(email: string) {
 }
 
 module.exports = {
-    createNewUser,
-    getUserByEmail,
-    getUserById,
-    getUsers,
-    deleteUserByEmail
-}
+  createNewUser,
+  getUserByEmail,
+  getUserById,
+  getUsers,
+  deleteUserByEmail,
+};
