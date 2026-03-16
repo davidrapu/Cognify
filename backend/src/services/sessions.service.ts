cc1fa625748426c6ad61766ba98b0dfec2b7984c
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-16 04:05:53 +0000

    refactor: clean up session service code and improve error handling in addNewSession function

--- backend/src/services/sessions.service.ts
+++ backend/src/services/sessions.service.ts
@@ -1,7 +1,11 @@
+import { HttpError } from "../types/errorsType";
 import type dataTypes = require("../types/dataTypes");
 // import type errorsType = require("../types/errorsType");
+const {
+  createGameSession,
+} = require("../database/repositories/gameSession.repository");
 
-const data: string[] = ['po']
+const data: string[] = ["po"];
 
 async function getSessionsData(userId: number) {
   // check if that user ID exists in the data file, showing if the user has any sessions recorded
@@ -17,25 +21,30 @@ async function getSessionsData(userId: number) {
 }
 
 async function getSessionDataById(userId: number, sessionId: number) {
-    // check if that user ID exists in the data file, showing if the user has any sessions recorded
-    // if it does return the session with that session ID
-    // otherwise throw an error saying no session found for that user ID and session ID
-    // const session = data.sessions.find((session: any) => session.userId === userId && session.id === sessionId);
-    // if (!session) {
-    //   const err: errorsType.HttpError = new Error("No session found for the given user ID and session ID");
-    //   err.status = 404;
-    //   throw err;
-    // }
-    return 0;
+  // check if that user ID exists in the data file, showing if the user has any sessions recorded
+  // if it does return the session with that session ID
+  // otherwise throw an error saying no session found for that user ID and session ID
+  // const session = data.sessions.find((session: any) => session.userId === userId && session.id === sessionId);
+  // if (!session) {
+  //   const err: errorsType.HttpError = new Error("No session found for the given user ID and session ID");
+  //   err.status = 404;
+  //   throw err;
+  // }
+  return 0;
 }
 
-async function addNewSession(sessionData: dataTypes.SessionData) {
-  // Logic to add a new session to the data file
-  // data.sessions.push(sessionData);
+async function addNewSession(sessionData: dataTypes.SessionDataType, userId: string) {
+  try {
+    await createGameSession(sessionData, userId);
+  } catch (error) {
+    const err: HttpError = error instanceof Error ? error : new Error(String(error));
+    err.status = 500;
+    throw err;
+  }
 }
 
 module.exports = {
   getSessionsData,
   getSessionDataById,
-  addNewSession
+  addNewSession,
 };
