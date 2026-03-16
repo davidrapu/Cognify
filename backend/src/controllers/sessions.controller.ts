087095b4ea8d2de14f6750eeec52a102195e47ad
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-16 04:01:29 +0000

    refactor: improve code formatting and structure in sessions.controller.ts

--- backend/src/controllers/sessions.controller.ts
+++ backend/src/controllers/sessions.controller.ts
@@ -1,8 +1,11 @@
 import type { Request, Response, NextFunction } from "express";
 import type dataTypes = require("../types/dataTypes");
-const {getSessionsData, getSessionDataById, addNewSession} = require("../services/sessions.service");
+const {
+  getSessionsData,
+  getSessionDataById,
+  addNewSession,
+} = require("../services/sessions.service");
 const { SessionSchema } = require("../schemas/session.schema");
-
 
 /**
  * @desc Create a new session and save it to the data file
@@ -11,12 +14,12 @@ const { SessionSchema } = require("../schemas/session.schema");
 async function addSession(req: Request, res: Response, next: NextFunction) {
   // Logic to add a new session
   try {
-        const sessionData: dataTypes.SessionData = SessionSchema.parse(req.body);
-        await addNewSession(sessionData);
-        res.status(201).json({ message: "Session created successfully" });
-    } catch (error) {
-        next(error);
-    }
+    const sessionData: dataTypes.SessionDataType = SessionSchema.parse(req.body);
+    await addNewSession(sessionData, req.user);
+    res.status(201).json({ message: "Session created successfully" });
+  } catch (error) {
+    next(error);
+  }
 }
 
 /**
@@ -28,7 +31,9 @@ async function getSessions(req: Request, res: Response, next: NextFunction) {
     // Call the service to get all the sessions based of the user ID
     const userId = req.user.id;
     const sessions = await getSessionsData(userId);
-    res.status(200).json({message: "Sessions retrieved successfully", data: sessions});
+    res
+      .status(200)
+      .json({ message: "Sessions retrieved successfully", data: sessions });
   } catch (error) {
     next(error);
   }
@@ -40,12 +45,14 @@ async function getSessions(req: Request, res: Response, next: NextFunction) {
  */
 async function getSession(req: Request, res: Response, next: NextFunction) {
   // Call the service to get all the sessions based of the user ID and session ID from the request parameters
-  try{
+  try {
     const userId = req.user.id;
     const sessionId = parseInt(req.params?.id as string);
     const session = await getSessionDataById(userId, sessionId);
-    res.status(200).json({message: "Session retrieved successfully", data: session});
-  }catch (error) {
+    res
+      .status(200)
+      .json({ message: "Session retrieved successfully", data: session });
+  } catch (error) {
     next(error);
   }
 }
