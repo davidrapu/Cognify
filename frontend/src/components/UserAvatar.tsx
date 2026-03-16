9ef9df10c9c455a29a040cc403819c2b0c9991a6
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-16 19:37:00 +0000

    Refactors Games page to use UserAvatar component and removes unused Avatar imports

--- frontend/src/components/UserAvatar.tsx
+++ frontend/src/components/UserAvatar.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "@/contexts/AuthContext/AuthContext";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {User} from './icons'
 
 
 export default function UserAvatar({size = 8}: {size?: number}) {
@@ -7,11 +8,11 @@ export default function UserAvatar({size = 8}: {size?: number}) {
   return (
     <Avatar className={`size-${size} rounded-lg`}>
       <AvatarImage
-        src={user.image || "USER_IMAGE"}
+        src={user.image}
         alt="user image"
       />
       <AvatarFallback className="bg-sidebar-primary text-sidebar-primary-foreground cursor-default">
-        {user.firstName?.charAt(0) + user.lastName?.charAt(0)}
+        {user.firstName?.charAt(0) + user.lastName?.charAt(0) || <User className="size-4" />}
       </AvatarFallback>
     </Avatar>
   );
