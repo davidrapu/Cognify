8387228919a3f9a23667cd8da3dc447fd31d404c
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-20 16:54:47 +0000

    new route. using params instead of query for game name

--- backend/src/routers/sessions.router.ts
+++ backend/src/routers/sessions.router.ts
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addSession, getSession, getSessions } = require("../controllers/sessions.controller");
+const { addSession, getSessions, getSessionsByGameName } = require("../controllers/sessions.controller");
 
 const router = express.Router();
 // @desc Get all sessions
@@ -7,9 +7,9 @@ const router = express.Router();
 // @route GET /sessions?gameName=gameName
 router.get("/", getSessions);
 
-// @desc Get a specific session by ID
-// @route GET /sessions/:id
-router.get("/:id", getSession);
+// @desc Get all sessions for a specific game type
+// @route GET /sessions/:gameName
+router.get("/:gameName", getSessionsByGameName);
 
 // @desc Create a new session
 // @route POST /sessions
