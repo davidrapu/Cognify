b0dbdd59ee7ce6b58597b1c96cc86d6a7e5b6536
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-22 21:47:58 +0000

    card match game metrics

--- frontend/src/hooks/useCardMatchReducer.ts
+++ frontend/src/hooks/useCardMatchReducer.ts
@@ -5,15 +5,32 @@ const initialState = {
     matchedCards: 0,
     totalAttempts: 0,
     gameLevel: "easy", // can be "easy", "medium", or "hard"
-    gameState: "active" // can be "active", intro, or "completed"
+    gameState: "active", // can be "active", intro, or "completed"
+    // Sent to the db after game completion
+    totalTime: [], // array to store time taken for each attempt (time taken to recall sequence) {time, correct:boolean}
+    totalCorrect: 0,
+    totalIncorrect: 0,
+    highestConsecutiveCorrect: 0, // highscore
+    // Gotten from db upon render of game home page
+    averageScore: 0, // gotten from backend upon render of game home page
+    averageAccuracy: 0, // gotten from backend upon render of game home page
+    highScore: 0, // gotten from backend upon render of game home page
 }
 
 export type CardMatchState = {
-    matchedCards: number;
-    totalAttempts: number;
-    gameLevel: string;
-    gameState: string;
-}
+  matchedCards: number;
+  totalAttempts: number;
+  gameLevel: string;
+  gameState: string;
+  totalTime: { time: number; correct: boolean }[];
+  totalCorrect: number;
+  totalIncorrect: number;
+  totalAllowedTries: number;
+  averageScore: number;
+  averageAccuracy: number;
+  highScore: number;
+  highestConsecutiveCorrect: number;
+};
 
 export type CardMatchAction =
     | { type: "increaseMatchedCards" }
