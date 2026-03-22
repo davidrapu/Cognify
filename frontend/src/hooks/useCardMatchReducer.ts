855abb54cd38aa67e933c3158dbf1fec2e28a39e
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-22 22:13:59 +0000

    bug for game logic

--- frontend/src/hooks/useCardMatchReducer.ts
+++ frontend/src/hooks/useCardMatchReducer.ts
@@ -25,7 +25,6 @@ export type CardMatchState = {
   totalTime: { time: number; correct: boolean }[];
   totalCorrect: number;
   totalIncorrect: number;
-  totalAllowedTries: number;
   averageScore: number;
   averageAccuracy: number;
   highScore: number;
