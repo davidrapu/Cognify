a5baf45ce1bb0593f89954cd33fc06aa7b979eb4
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-03-26 04:21:23 +0000

    better reusable component design

--- frontend/src/components/HeartDisplay.tsx
+++ frontend/src/components/HeartDisplay.tsx
@@ -1,12 +1,27 @@
 import { Heart } from "@/components/icons";
 
-export default function HeartDisplay({ filled }: { filled: boolean }) {
+export default function HeartDisplay({
+  numberOfFilledHearts,
+  innerColor,
+  outerColor,
+  length
+}: {
+  numberOfFilledHearts?: number;
+  innerColor?: string;
+  outerColor?: string;
+  length: number;
+}) {
   return (
-    <Heart
-      className="size-7"
-      fill={filled ? "var(--primary-foreground)" : "none"}
-      stroke="var(--primary-foreground)"
-      strokeWidth={2}
-    />
+    <div className="flex gap-x-2 flex-row-reverse">
+      {Array.from({ length: length }).map((_, index) => (
+        <Heart
+          key={index}
+          className="size-7"
+          fill={index < (numberOfFilledHearts ?? 0) ? innerColor || "var(--primary-foreground)" : "none"}
+          stroke={outerColor || "var(--primary-foreground)"}
+          strokeWidth={2}
+        />
+      ))}
+    </div>
   );
 }
