5ab5fcea868bb85aefe1f20553a6464947774186
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-04-02 01:22:13 +0100

    Updates input model and prediction fields for API
    Expands the input data schema to include new cognitive metrics and aligns the prediction logic with updated model features. Improves prediction clarity by renaming output fields and adds debug logging for easier troubleshooting.

--- ml-service/main.py
+++ ml-service/main.py
@@ -8,21 +8,30 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model = joblib.load(os.path.join(BASE_DIR, "cognify_model.pkl"))
 app = FastAPI()
 
+# "reaction_score","game_score","accuracy","stroop_error_rate","stroop_accuracy","go_nogo_accuracy","go_nogo_error_rate","memory_score","attention_score","problem_solving_score"
 class GameData(BaseModel):
-    avg_reaction_time: float
-    reaction_time_std: float
-    memory_score: float
+    reaction_score: float
+    game_score: float
     accuracy: float
     stroop_error_rate: float
+    stroop_accuracy: float
     go_nogo_accuracy: float
-
+    go_nogo_error_rate: float
+    memory_score: float
+    attention_score: float
+    problem_solving_score: float
 @app.post("/predict")
 def predict_cognitive_score(data: GameData):
+    print(f"Received data: {data}")
     try:
-        features = np.array([[data.avg_reaction_time, data.reaction_time_std, data.memory_score,
-                              data.accuracy, data.stroop_error_rate, data.go_nogo_accuracy]])
-        predicted_score = model.predict(features)[0]
+        features = np.array([[data.reaction_score, data.game_score, data.accuracy, data.stroop_error_rate, data.stroop_accuracy, data.go_nogo_accuracy, data.go_nogo_error_rate, data.memory_score, data.attention_score, data.problem_solving_score]])
+
+        predicted_score = int(model.predict(features)[0])
+
         label_map = {0: 'Low', 1: 'Moderate', 2: 'High'}
-        return {"predicted_cognitive_score": label_map.get(predicted_score, 'Unknown'), "predicted_score_value": predicted_score}
+
+        return {"predicted_cognitive_level": label_map.get(predicted_score, 'Unknown'), "predicted_score_value": predicted_score}
+    
     except Exception as e:
+        print(f"Error occurred: {e}")
         raise HTTPException(status_code=500, detail=str(e))
