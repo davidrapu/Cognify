11cef1245c5807f6c8677757357598eaa908946a
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-04-04 04:42:32 +0100

    logging all errors

--- backend/src/middleware/error.ts
+++ backend/src/middleware/error.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response, type NextFunction } from 'express';
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log("Full error:", err); // Log the entire error object for debugging
   if (err.status) {
     res.status(err.status).send({ message: err.message });
   } else {
