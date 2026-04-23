1c901dbea07c56468fe315f919b6a74685bd2dfa
David Rapu <168489960+davidrapu@users.noreply.github.com>
2026-04-23 01:14:35 +0100

    refactor: comment out debug log in verifyAnswer function

--- frontend/src/utils/VerifyAnswer.ts
+++ frontend/src/utils/VerifyAnswer.ts
@@ -246,7 +246,7 @@ export function verifyAnswer(
     case "city": {
       if (!answer || typeof answer !== "string") return 0;
       const correct = userInput.toLocaleLowerCase() === answer.toLocaleLowerCase();
-      console.log('correct', correct);
+      // console.log('correct', correct);
       if (correct) {
         increaseCategory("language", points);
         return points;
